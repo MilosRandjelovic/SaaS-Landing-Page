01afa2fa0b833dae380409118c27dbe1372dcf19
MilosRandjelovic <mrandjelovic1979@gmail.com>
2024-09-30 13:51:56 +0200

    create faq, testimonials, download and footer section

--- saas_landing_page/src/App.jsx
+++ saas_landing_page/src/App.jsx
@@ -1,7 +1,22 @@
-export default function App() {
+import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+
+
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      
+      
+      
+    
+    
+      
+      
+    </main>
+  );
+};
+
+export default App;
